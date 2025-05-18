69d07c78acd9f618b495756b56e24deb4054a5a7
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2025-05-18 15:34:14 +0300

    initial commit

---
+++ src/main/java/io/spring/movie_recommender/MovieRecommenderApiApplication.java
@@ -0,0 +1,13 @@
+package io.spring.movie_recommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommenderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/movie_recommender/MovieRecommenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.movie_recommender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
